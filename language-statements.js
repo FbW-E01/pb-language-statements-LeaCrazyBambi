958679ac287945f4362759a80b6d5520d150ee24
LeaCrazyBambi <lheynitz@gmail.com>
2021-03-30 10:22:34 +0200

    I hope this is what you meant with the excercise

---
+++ language-statements.js
@@ -0,0 +1,6 @@
+onsole.log("Hello World");
+// console.log("This is my first js file");
+
+
+// ReferenceError: onsole is not defined
+//     at Object.<anonymous> (/home/dci/projects/pb-language-statements-LeaCrazyBambi/language-statements.js:1:1)
